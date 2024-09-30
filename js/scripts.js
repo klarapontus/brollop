48930387bab9934306387eef0cd14c8d2f64e478
Pontus Nordqvist <p.nordq@gmail.com>
2024-09-30 16:26:13 +0200

    feat: made more progress

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        if (MD5($('#invite_code').val()) !== '1bfdf104aa5a20c04e258834266c79d8'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwDGbTBbFDn9RY3qxp84IkDm6PjEm78946my3NY0fi7mWOydKSj61IYnzICIi5jM0N3/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
